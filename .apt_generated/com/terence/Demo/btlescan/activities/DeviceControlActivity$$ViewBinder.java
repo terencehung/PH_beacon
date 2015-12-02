2329bcffb4c6171288d185f3493dee913bb31d42
terence.hung <terence.hung>
2015-12-02 18:18:22 +0800

    edit1102

--- .apt_generated/com/terence/Demo/btlescan/activities/DeviceControlActivity$$ViewBinder.java
+++ .apt_generated/com/terence/Demo/btlescan/activities/DeviceControlActivity$$ViewBinder.java
@@ -8,26 +8,26 @@ import butterknife.ButterKnife.ViewBinder;
 public class DeviceControlActivity$$ViewBinder<T extends com.terence.Demo.btlescan.activities.DeviceControlActivity> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131296341, "field 'mDataAsArray'");
-    target.mDataAsArray = finder.castView(view, 2131296341, "field 'mDataAsArray'");
-    view = finder.findRequiredView(source, 2131296339, "field 'mGattUUIDDesc'");
-    target.mGattUUIDDesc = finder.castView(view, 2131296339, "field 'mGattUUIDDesc'");
     view = finder.findRequiredView(source, 2131296343, "field 'mGattServicesList'");
     target.mGattServicesList = finder.castView(view, 2131296343, "field 'mGattServicesList'");
-    view = finder.findRequiredView(source, 2131296338, "field 'mGattUUID'");
-    target.mGattUUID = finder.castView(view, 2131296338, "field 'mGattUUID'");
     view = finder.findRequiredView(source, 2131296340, "field 'mDataAsString'");
     target.mDataAsString = finder.castView(view, 2131296340, "field 'mDataAsString'");
+    view = finder.findRequiredView(source, 2131296338, "field 'mGattUUID'");
+    target.mGattUUID = finder.castView(view, 2131296338, "field 'mGattUUID'");
+    view = finder.findRequiredView(source, 2131296339, "field 'mGattUUIDDesc'");
+    target.mGattUUIDDesc = finder.castView(view, 2131296339, "field 'mGattUUIDDesc'");
+    view = finder.findRequiredView(source, 2131296341, "field 'mDataAsArray'");
+    target.mDataAsArray = finder.castView(view, 2131296341, "field 'mDataAsArray'");
     view = finder.findRequiredView(source, 2131296335, "field 'mConnectionState'");
     target.mConnectionState = finder.castView(view, 2131296335, "field 'mConnectionState'");
   }
 
   @Override public void unbind(T target) {
-    target.mDataAsArray = null;
-    target.mGattUUIDDesc = null;
     target.mGattServicesList = null;
-    target.mGattUUID = null;
     target.mDataAsString = null;
+    target.mGattUUID = null;
+    target.mGattUUIDDesc = null;
+    target.mDataAsArray = null;
     target.mConnectionState = null;
   }
 }
--- .apt_generated/com/terence/Demo/btlescan/activities/MainActivity$$ViewBinder.java
+++ .apt_generated/com/terence/Demo/btlescan/activities/MainActivity$$ViewBinder.java
@@ -8,23 +8,23 @@ import butterknife.ButterKnife.ViewBinder;
 public class MainActivity$$ViewBinder<T extends com.terence.Demo.btlescan.activities.MainActivity> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
+    view = finder.findRequiredView(source, 2131296351, "field 'mList'");
+    target.mList = finder.castView(view, 2131296351, "field 'mList'");
     view = finder.findRequiredView(source, 2131296349, "field 'mTvItemCount'");
     target.mTvItemCount = finder.castView(view, 2131296349, "field 'mTvItemCount'");
-    view = finder.findRequiredView(source, 2131296346, "field 'mTvBluetoothStatus'");
-    target.mTvBluetoothStatus = finder.castView(view, 2131296346, "field 'mTvBluetoothStatus'");
     view = finder.findRequiredView(source, 2131296345, "field 'mTvBluetoothLeStatus'");
     target.mTvBluetoothLeStatus = finder.castView(view, 2131296345, "field 'mTvBluetoothLeStatus'");
     view = finder.findRequiredView(source, 16908292, "field 'mEmpty'");
     target.mEmpty = view;
-    view = finder.findRequiredView(source, 2131296351, "field 'mList'");
-    target.mList = finder.castView(view, 2131296351, "field 'mList'");
+    view = finder.findRequiredView(source, 2131296346, "field 'mTvBluetoothStatus'");
+    target.mTvBluetoothStatus = finder.castView(view, 2131296346, "field 'mTvBluetoothStatus'");
   }
 
   @Override public void unbind(T target) {
+    target.mList = null;
     target.mTvItemCount = null;
-    target.mTvBluetoothStatus = null;
     target.mTvBluetoothLeStatus = null;
     target.mEmpty = null;
-    target.mList = null;
+    target.mTvBluetoothStatus = null;
   }
 }
